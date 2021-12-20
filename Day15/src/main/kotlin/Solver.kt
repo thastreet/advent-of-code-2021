33a47af83a18c254c18fd6b1c19cd6d5e1c2e0a2
Mathieu Larue <mlarue@mirego.com>
2021-12-19 22:51:45 -0500

    Day 15 part 2

--- Day15/src/main/kotlin/Solver.kt
+++ Day15/src/main/kotlin/Solver.kt
@@ -1,3 +1,5 @@
+import java.util.*
+
 class Solver {
     fun solvePart1(grid: List<List<Int>>): Int =
         solve(grid)
@@ -40,28 +42,30 @@ class Solver {
 
         val dist = mutableMapOf<Point, Int>()
         val prev = mutableMapOf<Point, Point>()
-        val nodes = mutableSetOf<Point>()
+        val nodes = PriorityQueue<Point> { o1, o2 ->
+            dist.getValue(o1).compareTo(dist.getValue(o2))
+        }
 
         for (y in 0 until height) {
             for (x in 0 until width) {
                 val point = Point(x, y)
                 dist[point] = if (x == 0 && y == 0) 0 else Int.MAX_VALUE
-                nodes.add(point)
             }
         }
+
+        nodes.add(Point(0, 0))
 
         val destination = Point(width - 1, height - 1)
 
         while (nodes.isNotEmpty()) {
-            val current = nodes.minByOrNull { dist.getValue(it) } ?: break
-            nodes.remove(current)
+            val current = nodes.remove()
 
             if (current == destination) break
 
-            val top: Point? = Point(current.x, current.y - 1).takeIf { it.y >= 0 && nodes.contains(it) }
-            val left: Point? = Point(current.x - 1, current.y).takeIf { it.x >= 0 && nodes.contains(it) }
-            val right: Point? = Point(current.x + 1, current.y).takeIf { it.x <= width - 1 && nodes.contains(it) }
-            val bottom: Point? = Point(current.x, current.y + 1).takeIf { it.y <= height - 1 && nodes.contains(it) }
+            val top: Point? = Point(current.x, current.y - 1).takeIf { it.y >= 0 && dist[it] == Int.MAX_VALUE }
+            val left: Point? = Point(current.x - 1, current.y).takeIf { it.x >= 0 && dist[it] == Int.MAX_VALUE }
+            val right: Point? = Point(current.x + 1, current.y).takeIf { it.x <= width - 1 && dist[it] == Int.MAX_VALUE }
+            val bottom: Point? = Point(current.x, current.y + 1).takeIf { it.y <= height - 1 && dist[it] == Int.MAX_VALUE }
 
             listOfNotNull(top, left, right, bottom).forEach {
                 val newDist = dist.getValue(current) + grid[it.y][it.x]
@@ -70,6 +74,8 @@ class Solver {
                     dist[it] = newDist
                     prev[it] = current
                 }
+
+                nodes.add(it)
             }
         }
 
